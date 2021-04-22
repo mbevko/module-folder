5021d62bb620baadc8b9c2beb5ccbeb82903957c
Michael Evko <mbevko@gmail.com>
2021-04-22 07:10:47 -0400

    hamburger menu

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 let left = document.querySelector(".left");
 let right = document.querySelector(".right");
+let hamburgerMenu = document.querySelector(".secondary_menu")
 
 window.addEventListener('scroll', () => {
-    if(scrollY >= 123){
+    
+    if(scrollY >= 125){
         left.style.cssText = "transform: translateX(0); opacity: 1;"
         right.style.cssText = "transform: translateX(0); opacity: 1;" 
+        hamburgerMenu.style.cssText = "opacity: 1; pointer-events: all"
+    } else if(scrollY < 125){
+        hamburgerMenu.style.cssText = "opacity: 0; pointer-events: none"
     }
     console.log(scrollY);
 })
